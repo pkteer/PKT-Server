967b68860b00b1f8b47ed387e1cf435cf95986d2
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-01-18 17:35:17 +0200

    added PKTEER_SECRET

--- init.sh
+++ init.sh
@@ -2,11 +2,14 @@
 # Set up iptables rules
 echo "Cjdns port is: "
 echo $(cat /server/cjdns/cjdroute.conf | grep bind | awk '/"0\.0\.0\.0:/' | cut -d':' -f3 | cut -d'"' -f1)
+echo "Checking PKTEER_SECRET: $PKTEER_SECRET"
+echo "Generating seed..."
+echo "/server/cjdns/cjdroute.conf|$PKTEER_SECRET" | sha256sum | /server/cjdns/cjdroute --genconf-seed
 echo "Setting up iptables rules"
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth0 -o tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i tun0 -o eth0 -j ACCEPT
-echo "sysctl..."
+echo "Forwarding enabled..."
 sysctl -w net.ipv4.ip_forward=1
 echo "Starting cjdns..."
 /server/cjdns/cjdroute < /server/cjdns/cjdroute.conf
