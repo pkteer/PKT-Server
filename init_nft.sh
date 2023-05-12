e7811788f4fe142c6799b9cdb4b4691e9604896f
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-05-12 11:46:14 +0300

    update rules

--- init_nft.sh
+++ init_nft.sh
@@ -5,8 +5,8 @@ adduser --no-create-home --disabled-password --disabled-login speedtest
 ## Switch the firewall
 nft -f ./pfi.nft
 
-#ip rule add fwmark 0xfc table cjdns
-#ip rule add fwmark 0xcf table nocjdns
+ip rule add fwmark 0xfc table cjdns
+ip rule add fwmark 0xcf table nocjdns
 
 tc qdisc add dev tun0 root handle 1:0 hfsc default ffff
     tc class add dev tun0 parent 1:0 classid 1:1 hfsc ls m2 100mbit ul m2 100mbit
