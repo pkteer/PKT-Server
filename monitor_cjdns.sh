a1a69082d03920feac7e13e13e593aa1fdf7c755
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-05-15 17:15:15 +0300

    bug fixes

--- monitor_cjdns.sh
+++ monitor_cjdns.sh
@@ -4,6 +4,24 @@ cexec="/server/cjdns/contrib/python/cexec"
 lsLimitFree=100kbit
 lsLimitPaid=950mbit
 
+get_hex_from_ip() {
+    decimal_to_hex() {
+        printf "%02x" "$1"
+    }
+    # Extract the last two octets of the source IP address
+    last_two_parts=$(echo "$1" | awk -F. '{ print $(NF-1), $NF }')
+
+    part1=$(echo "$last_two_parts" | awk '{print $1}')
+    part2=$(echo "$last_two_parts" | awk '{print $2}')
+
+    # Convert each part to hexadecimal
+    hex_part1=$(decimal_to_hex "$part1")
+    hex_part2=$(decimal_to_hex "$part2")
+
+    # Concatenate the hexadecimal parts
+    printf "${hex_part1}${hex_part2}"
+}
+
 # Listen for clients
 while true; do
     output=$($cexec 'IpTunnel_listConnections()')
@@ -11,8 +29,9 @@ while true; do
     #"
     conn_ids=$(echo "$output" | jq -r '.connections[]')
     if [ -z "$conn_ids" ]; then
-        echo "connections array is empty"
-        exit 1
+        echo "No connections found"
+        sleep 2
+        continue
     fi
     # Loop over the connection IDs and extract the IPv4 address for each one
     for conn_id in $conn_ids; do
@@ -20,25 +39,18 @@ while true; do
         output=$(echo "$output" | sed "s/'/\"/g")
         ipv4_addr=$(echo "$output" | jq -r '.ip4Address')
         echo "Connection $conn_id has IPv4 address $ipv4_addr"
+        HEX=$(get_hex_from_ip "$ipv4_addr")
+
+        # Create a tc class for the source IP address
+        if [ "$PAID" = true ]; then
+            echo "PAID $ipv4_addr"
+            tc class replace dev $DEVICE parent 1:fffe classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
+            nft add element pfi m_client_leases { $ipv4_addr : "1:$HEX" }
+        else
+            echo "FREE $ipv4_addr"
+            tc class delete dev $DEVICE parent 1:fffe classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
+        fi
     done
-
-    # Extract the last two octets of the source IP address
-    OCTETS=$(echo "$ipv4_addr" | awk -F '.' '{print $3 $4}')
-
-    # Convert the octets to hex
-    HEX=$(printf '%02x' $OCTETS)
-
-    # Create a tc class for the source IP address
-    echo "Creating tc class for $ipv4_addr"
-    if [ "$PAID" = true ]; then
-        echo "Give unlimited bandwidth to $ipv4_addr"
-        echo "dev $DEVICE parent 1:ffff classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid"
-        tc class replace dev $DEVICE parent 1:ffff classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
-        nft add element pfi m_client_leases { $ipv4_addr : "1:$HEX" }
-    else
-        tc class delete dev $DEVICE parent 1:ffff classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
-        nft delete element pfi m_client_leases { $ipv4_addr : "1:$HEX" }
-    fi
 
     sleep 5
 done
