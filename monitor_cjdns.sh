67dfa60c22ec17033513a7de3235bcf0792280d0
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-05-16 16:09:33 +0300

    update script

--- monitor_cjdns.sh
+++ monitor_cjdns.sh
@@ -23,34 +23,31 @@ get_hex_from_ip() {
 }
 
 # Listen for clients
-while true; do
-    output=$($cexec 'IpTunnel_listConnections()')
+output=$($cexec 'IpTunnel_listConnections()')
+output=$(echo "$output" | sed "s/'/\"/g")
+#"
+conn_ids=$(echo "$output" | jq -r '.connections[]')
+if [ -z "$conn_ids" ]; then
+    echo "No connections found"
+    sleep 2
+    continue
+fi
+# Loop over the connection IDs and extract the IPv4 address for each one
+for conn_id in $conn_ids; do
+    output=$($cexec 'IpTunnel_showConnection('$conn_id')')
     output=$(echo "$output" | sed "s/'/\"/g")
-    #"
-    conn_ids=$(echo "$output" | jq -r '.connections[]')
-    if [ -z "$conn_ids" ]; then
-        echo "No connections found"
-        sleep 2
-        continue
+    ipv4_addr=$(echo "$output" | jq -r '.ip4Address')
+    
+    echo "Connection $conn_id has IPv4 address $ipv4_addr"
+    HEX=$(get_hex_from_ip "$ipv4_addr")
+
+    # Create a tc class for the source IP address
+    if [ "$1" = "$ipv4_addr" ] && [ "$2" = "true" ]; then
+        echo "PAID $ipv4_addr"
+        tc class replace dev $DEVICE parent 1:fffe classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
+        nft add element pfi m_client_leases { $ipv4_addr : "1:$HEX" }
+    elif [ "$1" = "$ipv4_addr" ] && [ "$2" = "false" ]; then
+        echo "FREE $ipv4_addr"
+        tc class delete dev $DEVICE parent 1:fffe classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
     fi
-    # Loop over the connection IDs and extract the IPv4 address for each one
-    for conn_id in $conn_ids; do
-        output=$($cexec 'IpTunnel_showConnection('$conn_id')')
-        output=$(echo "$output" | sed "s/'/\"/g")
-        ipv4_addr=$(echo "$output" | jq -r '.ip4Address')
-        echo "Connection $conn_id has IPv4 address $ipv4_addr"
-        HEX=$(get_hex_from_ip "$ipv4_addr")
-
-        # Create a tc class for the source IP address
-        if [ "$PAID" = true ]; then
-            echo "PAID $ipv4_addr"
-            tc class replace dev $DEVICE parent 1:fffe classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
-            nft add element pfi m_client_leases { $ipv4_addr : "1:$HEX" }
-        else
-            echo "FREE $ipv4_addr"
-            tc class delete dev $DEVICE parent 1:fffe classid 1:$HEX hfsc ls m2 $lsLimitPaid ul m2 $lsLimitPaid
-        fi
-    done
-
-    sleep 5
 done
