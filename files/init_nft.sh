41ff9892df031d73157160d48a83e3be21fc18e2
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-07-20 16:41:52 +0300

    remove ul

--- files/init_nft.sh
+++ files/init_nft.sh
@@ -4,5 +4,5 @@ nft -f ./pfi.nft
 
 tc qdisc replace dev tun0 root handle 1:0 hfsc default ffff
     tc class replace dev tun0 parent 1:0 classid 1:fffe hfsc ls m2 950mbit 
-    tc class replace dev tun0 parent 1:0 classid 1:ffff hfsc ls m2 1mbit ul m2 1mbit
+    tc class replace dev tun0 parent 1:0 classid 1:ffff hfsc ls m2 1mbit
         tc qdisc replace dev tun0 parent 1:ffff handle ffff: cake
