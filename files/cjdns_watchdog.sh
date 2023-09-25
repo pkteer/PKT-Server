549d4c27291d4fe2a758bc5641582e2d4bdd78f8
dkoukoul <dimitris.koukoulakis@gmail.com>
2023-09-25 12:51:52 +0300

    restart

--- files/cjdns_watchdog.sh
+++ files/cjdns_watchdog.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
+restart() {
+    # Kill cjdroute
+    pkill cjdroute
+    if pidof cjdroute > /dev/null; then
+        pkill -9 cjdroute
+    fi
+
+    # Kill node
+    kill $(ps aux | pgrep -x node)
+
+    # Set CAP_NET_ADMIN to cjdroute
+    setcap cap_net_admin=eip /server/cjdns/cjdroute
+
+    # Launch cjdns
+    su - cjdns <<EOF
+/server/cjdns/cjdroute < /data/cjdroute.conf
+EOF
+
+    # Launch node
+    if [ -e /data/env/vpnprice ]; then
+        export PKTEER_PREMIUM_PRICE=$(cat /data/env/vpnprice)
+    else
+        # Default price
+        export PKTEER_PREMIUM_PRICE=10
+    fi
+    node /server/anodevpn-server/index.js &
+
+    sleep 2
+}
+
 while true; do
     # use timeout in case cjdroute is not running
     if ! timeout 2s /server/cjdns/tools/cexec 'ping()' | grep -q pong; then
         echo "cjdns is not running, restarting..."
-        pkill cjdroute
-        if pidof cjdroute > /dev/null; then
-            pkill -9 cjdroute
-        fi
-        # Set CAP_NET_ADMIN to cjdroute
-        setcap cap_net_admin=eip /server/cjdns/cjdroute
-        # Launch cjdns
-su - cjdns <<EOF
-/server/cjdns/cjdroute < /data/cjdroute.conf
-
-EOF
-        # kill anodevpn-server, so it restarts
-        kill $(ps aux | pgrep -x node)
-        
+        restart
     else
         echo "$(date): cjdns is running."
     fi
@@ -25,20 +42,14 @@ EOF
     node=$(ps aux | pgrep -x node)
     if [ -z "$node" ]; then
         echo "anodevpn-server is not running, restarting..."
-        if [ -e /data/env/vpnprice ]; then
-            export PKTEER_PREMIUM_PRICE=$(cat /data/env/vpnprice)
-        else
-            # Default price
-            export PKTEER_PREMIUM_PRICE=10
-        fi
-        node /server/anodevpn-server/index.js &
+        restart
     else
         response=$(curl --write-out %{http_code} --silent --connect-timeout 5 --output /dev/null http://localhost:8099)
         if [ "$response" -eq 404 ]; then
             echo "$(date): anodevpn-server is running."
         else
             echo "anodevpn-server is running but not responding, restarting..."
-            kill $(ps aux | pgrep -x node)
+            restart
         fi
     fi
     sleep 5
